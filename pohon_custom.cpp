5b6576a810c7301c108ff77c0438517cb3cd89d5
ItsWHirly <muhamadaidilsetyantoro@gmail.com>
2026-07-31 21:59:21 +0700

    day 4: belajar for

---
+++ pohon_custom.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main (){
+    int daun = 3;
+    int i;
+    int j;
+    std::cout << " " << * (3 % 2)
+    for (i=1; i<=daun; i++) {
+        if (i % 3 == 0) {
+            for (j=1; j<=i; j++) {
+                std::cout<<"*";
+            }
+        }
+    std::cout<<std::endl;
+    }
+}
