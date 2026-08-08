970c80fef8a142cea31d1752e24c56bb9ef5c237
ItsWHirly <muhamadaidilsetyantoro@gmail.com>
2026-08-08 19:31:40 +0700

    day 9: menambahkan UI untuk proyek sistem akun

---
+++ account_system/ui_accsystem.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+
+int main (){
+    int pilihan;
+    
+    std::cout << "=== SELAMAT DATANG DI WHIRLY.WEB ===" << std::endl;
+    std::cout << "1. Registrasi Akun" << std::endl;
+    std::cout << "2. Masuk ke akun" << std::endl;
+    std::cout << "3. Hapus akun" << std::endl;
+    std::cout << "4. Ganti Sandi Akun" << std::endl;
+    std::cout << "5. Informasi" << std::endl;
+    std::cout << "====================================" << std::endl;
+    std::cout << "pilih: ";
+    std::cin >> pilihan;
+
+    if (pilihan == 1){
+        std::cout<<"masih proses ygy";
+    }
+    if (pilihan == 2){
+        std::cout<<"masih proses juga";
+    }
+    if (pilihan == 3){
+        std::cout<<"ini juga proses";
+    }
+    if (pilihan == 4){
+        std::cout<<"sabar yahh";
+    }
+    if (pilihan == 5){
+        std::cout<<"tunggu aja ehhehe";
+    }
+    else {
+        std::cout<<"ora valid kocak";
+    }
+}
